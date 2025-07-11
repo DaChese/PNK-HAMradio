1096d2e92914d3ee14bb2027ea5ad72746593bed
DaChese <mealdo@umich.edu>
2025-07-11 16:57:29 +0000

    edited the start up script/wrapper

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,9 @@
-add start up wrapper
+#!/usr/bin/env bash
+# PNK Dashboard startup script
+# Bring up all services in detached mode
+
+echo "Starting PNK services..."
+docker compose up -d
+
+echo "Here are the running containers:"
+docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
