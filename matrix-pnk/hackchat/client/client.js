74ff4ff738c35175bf47524d37732540b17561bc
DaChese <mealdo@umich.edu>
2025-08-11 17:52:35 +0000

    updated client.js and monitoring logs for services

--- matrix-pnk/hackchat/client/client.js
+++ matrix-pnk/hackchat/client/client.js
@@ -344,61 +344,57 @@ function join(channel) {
     // For https://hack.chat/
     ws = new WebSocket('wss://hack.chat/chat-ws');
   } else {
-    // for local installs
-    var protocol = location.protocol === 'https:' ? 'wss:' : 'ws:'
-    // if you changed the port during the server config, change 'wsPath'
-    // to the new port (example: ':8080')
-    // if you are reverse proxying, change 'wsPath' to the new location
-    // (example: '/chat-ws')
-    var wsPath = ':6060';
-    ws = new WebSocket(protocol + '//' + document.domain + wsPath);
-  }
+    const proto = location.protocol === 'https:' ? 'wss' : 'ws';
+    const host = location.hostname;
 
-  var wasConnected = false;
+    // Try /chat-ws first (matches upstream), fallback to root if needed
+    const tryUrls = [
+      `${proto}://${host}:6060/chat-ws`,
+      `${proto}://${host}:6060/`
+    ];
 
-  ws.onopen = function () {
-    var shouldConnect = true;
-    if (!wasConnected) {
-      if (location.hash) {
-        myNick = location.hash.substr(1);
-      } else {
-        var newNick = prompt('Nickname:', myNick);
-        if (newNick !== null) {
-          myNick = newNick;
+    let idx = 0;
+    const connect = () => {
+      const url = tryUrls[idx];
+      console.log('Hack.Chat: connecting to', url);
+      ws = new WebSocket(url);
+
+      ws.onopen = () => {
+        let shouldConnect = true;
+        if (location.hash) {
+          myNick = location.hash.substr(1);
         } else {
-          // The user cancelled the prompt in some manner
-          shouldConnect = false;
+          const newNick = prompt('Nickname:', myNick);
+          if (newNick !== null) myNick = newNick; else shouldConnect = false;
         }
-      }
-    }
+        if (myNick && shouldConnect) {
+          localStorageSet('my-nick', myNick);
+          send({ cmd: 'join', channel: channel, nick: myNick });
+        }
+      };
 
-    if (myNick && shouldConnect) {
-      localStorageSet('my-nick', myNick);
-      send({ cmd: 'join', channel: channel, nick: myNick });
-    }
+      ws.onclose = () => {
+        // if first URL failed immediately, try fallback once
+        if (idx === 0) {
+          idx = 1;
+          connect();
+          return;
+        }
+        pushMessage({ nick: '!', text: "Server disconnected. Attempting to reconnect . . ." });
+        setTimeout(connect, 2000);
+      };
 
-    wasConnected = true;
-  }
+      ws.onmessage = (message) => {
+        const args = JSON.parse(message.data);
+        const command = COMMANDS[args.cmd];
+        if (command) command.call(null, args);
+      };
+    };
 
-  ws.onclose = function () {
-    if (wasConnected) {
-      pushMessage({ nick: '!', text: "Server disconnected. Attempting to reconnect. . ." });
-    }
-
-    window.setTimeout(function () {
-      join(channel);
-    }, 2000);
-  }
-
-  ws.onmessage = function (message) {
-    var args = JSON.parse(message.data);
-    var cmd = args.cmd;
-    var command = COMMANDS[cmd];
-    if (command) {
-      command.call(null, args);
-    }
+    connect();
   }
 }
+
 
 var COMMANDS = {
   chat: function (args) {
